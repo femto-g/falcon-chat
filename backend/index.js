a19d980602fc397ebb901b73121ab7b8c4632a46
femto-g <olyb11@yahoo.com>
2023-01-03 17:19:29 -0500

    added client-server socket connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
+const http = require("http");
+const socket = require("socket.io");
+
+const httpServer = http.createServer(app);
+const io = new socket.Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true
+  }
+});
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.listen(port, function () {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
+// app.listen(3000); will not work here, as it creates a new HTTP serve
+httpServer.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
